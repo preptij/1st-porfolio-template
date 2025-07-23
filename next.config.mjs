332a442f8873b5ad4d5cea1490879a3fadc41314
preptij <100182614+preptij@users.noreply.github.com>
2025-07-23 01:48:53 -0400

    fix: rename next.config.ts to next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+// next.config.mjs
+import { type NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost'],
+  },
+  experimental: {
+    optimizePackageImports: ['framer-motion'],
+  },
+}
+
+export default nextConfig
